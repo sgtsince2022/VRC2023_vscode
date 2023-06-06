033f3808203808f55f1a4cd7a76154ac093dad5c
RETROGRADERZ <hereda.unknown@gmail.com>
2023-06-06 19:17:41 +0700

    initial commit by RETROGRADER / testing control system (but failed:D)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,87 @@
-#include <main.h>
+#include <Arduino.h>
+#include <PS2X_lib.h>
+#include "main.h"
+#include <EEB.h>
+
+PS2X VRC_PS2;
+DCMotor VRC_Motor;
+int pwm_left, pwm_right; bool dir_left, dir_right;
 
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(115200);
+    Serial.println("Mach khoi tao thanh cong!");
+    ps2_init();
+    VRC_Motor.Init();
+}
 
-}
 void loop() {
-  Serial.println("Hello SGT!");
+    ps2_ctrl();
+    analog_to_pwm_value();
+    delay(50);
+    VRC_Motor.Run(1, pwm_left, 0);
+    VRC_Motor.Run(2, pwm_right, dir_right);
 }
+
+void ps2_init() {
+    Serial.println("connecting to ps2..");
+
+    int err = -1;
+    for (int i = 0; i < 10; i++) {
+        delay(1000);
+        err = VRC_PS2.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
+        Serial.println("attempting..");
+    }
+
+    switch (err)
+    {
+    case 0:
+    Serial.println(" Ket noi tay cam PS2 thanh cong");
+    break;
+  case 1:
+    Serial.println(" LOI: Khong tim thay tay cam, hay kiem tra day ket noi vơi tay cam ");
+    break;
+  case 2:
+    Serial.println(" LOI: khong gui duoc lenh");
+    break;
+  case 3:
+    Serial.println(" LOI: Khong vao duoc Pressures mode ");
+    break;
+    }
+}
+
+void ps2_ctrl() {
+    VRC_PS2.read_gamepad(false, false);
+
+    if (VRC_PS2.Button(PSB_L1)) {
+        Serial.print("LY, RX: ");
+        Serial.print(VRC_PS2.Analog(PSS_LY), DEC); 
+        Serial.print(", "); Serial.println(VRC_PS2.Analog(PSS_RX));
+    }
+}
+
+void analog_to_pwm_value() {
+    byte v_LY = VRC_PS2.Analog(PSS_LY);
+    byte v_RX = VRC_PS2.Analog(PSS_RX);
+
+    if ((v_LY >= 0) && (v_LY <= 20)) {
+        pwm_left = MAX_PWM*1;
+        pwm_right = MAX_PWM*1;
+    } else {pwm_left = 0; pwm_right = 0;}
+
+    if ((v_LY >= 235) && (v_LY <= 255)) {
+        pwm_left = -MAX_PWM*1;
+        pwm_right = -MAX_PWM*1;
+    } else {pwm_left = 0; pwm_right = 0;}
+
+    if ((v_RX >= 0) && (v_RX <= 20)) {
+        pwm_left = MAX_PWM/2;
+        pwm_right = MAX_PWM/3*2;
+    } else {pwm_left = 0; pwm_right = 0;}
+
+    if ((v_RX >= 235) && (v_RX <= 255)) {
+        pwm_left = (int16_t) MAX_PWM/3*2;
+        pwm_right = (int16_t) MAX_PWM/3*2;
+        dir_right = 1;
+    } else {pwm_left = 0; pwm_right = 0;}
+    
+}   
--- src/main.h
+++ src/main.h
@@ -1,12 +1,17 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <Arduino.h>
-#include <MPU6050.h>
-#include <PS2X_lib.h>
-#include <Adafruit_PWMServoDriver.h>
-#include <EEB.h>
+#define PS2_DAT 12 // MISO
+#define PS2_CMD 13 // MOSI
+#define PS2_SEL 15 // SS
+#define PS2_CLK 14 // SLK
+#define pressures false
+#define rumble false
 
+void ps2_init();
+void ps2_ctrl();
+void analog_to_pwm_value();
 
+#define MAX_PWM 4096
 
 #endif
